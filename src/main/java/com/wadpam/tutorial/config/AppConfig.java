bf33fd29670059a2b1516d864dad0e2600dfd07e
sosandstrom <os@goldengekko.com>
2013-02-23 18:13:53 +0700

    Adding Spring-MVC

---
+++ src/main/java/com/wadpam/tutorial/config/AppConfig.java
@@ -0,0 +1,16 @@
+/*
+ * INSERT COPYRIGHT HERE
+ */
+
+package com.wadpam.tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author sosandstrom
+ */
+@Configuration
+public class AppConfig {
+
+}
